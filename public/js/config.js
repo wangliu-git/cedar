7c8d983d9ace44d03febb980a0f832de0f5cc21b
wangliu-git <1947389483@qq.com>
2020-06-22 09:54:14 +0800

    a

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 window.g = {
     AXIOS_TIMEOUT: 10000,
     // ApiUrl: 'http://localhost/cedar/api/', // 配置服务器地址,
-     ApiUrl: 'http://192.168.75.58/cedar/api/', // 配置服务器地址,
+    ApiUrl: 'http://192.168.75.58/cedar/api/', // 配置服务器地址,
     // ApiUrl: 'https://cedar.genowis.com/ceder/api/', // 配置服务器地址,
     getURL:'http://192.168.75.58/cedar/' ,  //获取图片地址
     // getURL:'https://cedar.genowis.com/ceder' ,  //获取图片地址
